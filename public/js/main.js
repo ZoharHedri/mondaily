212a64c0f68c2ffe44f351bc860d63e0bc8510b9
Amitay <Amitay@mail.com>
2018-06-18 18:26:18 +0300

    unfinished review feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,30 @@
 
 
-// setInterval(function updateStanding() {
-//     $.ajax({
-//         type: 'GET',
-//         url: '/standing/update'
-//     });
-// }, 1000 * 60 * 1 * 1);
+$('.post-review').on('click', () => {
+    let userId = JSON.parse(localStorage.getItem('user'));
+    if (!userId) {
+        alert('you are not connected');
+        return;
+    }
+    let reviewText = $(this).find('review-text').val();
+    let matchId = JSON.parse(localStorage.getItem('match'));
+})
+
+ajaxPostReview = (postReviewObject) => {
+    userId = postReviewObject.userId;
+
+    $.ajax({
+        type: 'POST',
+        url: '/reviews/add/' + userId,
+        data: postReviewObject,
+    })
+        .then(() => {
+
+        })
+
+}
+
+$.ajax({
+    method: 'GET',
+
+})
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const Router = express.Router();
+const Review = require('../model/reviewsModel');
+const Match = require();
+
+Router.post('/add/:userId', (req, res) => {
+    _userId = req.params.userId;
+    _reviewText = req.body.text;
+    _matchId = req.body.matchId;
+
+    let newReview = new Review({
+        userId: _userId,
+        reviewText: _reviewText,
+        matchId: _matchId
+    })
+
+    newReview.save()
+        .then((review) => {
+            res.render('reviews');
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+})
+
+Router.get('/match/:match_id', (req, res) => {
+
+})
+
+Router.get('/match/getMatch', (req, res) => {
+
+})
 
 Router.get('/', (req, res) => {
-
+    res.render('reviews');
 });
 
 module.exports = Router;
