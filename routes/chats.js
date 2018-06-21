966669f1928880f7c2ce3af9d6876f953c9ab282
zohar <yeoodahed@gmail.com>
2018-06-21 21:09:17 +0300

    finish chat

--- routes/chats.js
+++ routes/chats.js
@@ -6,4 +6,32 @@ Router.get('/', (req, res) => {
     res.render('chats/chat');
 });
 
+
+Router.post('/addChat', (req, res) => {
+    //console.log(req.body.matchid + ' ' + req.body.username + ' ' + req.body.text); good
+
+    Chat.create({
+        // match_id: req.body.matchid,
+        match_id: 1,
+        user_name: req.body.username,
+        text: req.body.text
+    }, function (err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('saved chat');
+        res.end();
+    });
+
+
+});
+
+
+Router.get('/getChat/:match_id', (req, res) => {
+    Chat.find({ match_id: req.params.match_id })
+        .then((chats) => {
+            res.send(chats);
+        })
+        .catch((err) => { throw err; })
+});
 module.exports = Router;
